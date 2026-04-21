7266b0c7e9aecc9cbb3b208dae2f24bf7d4caf5d
hal123w <haruto123c@gmail.com>
2026-04-21 14:25:17 +0900

    Ultra safety for images and models

--- sns/models.py
+++ sns/models.py
@@ -1,34 +1,32 @@
-from django.db import models
+from django.db import migrations, models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Post(models.Model):
-    # 投稿者（後でログイン機能と繋げるための大事な設定）
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # 投稿内容（140文字制限にしてみましょう）
-    content = models.CharField(max_length=140)
-    # 投稿日時（自動で今の時間が記録される）
+    content = models.TextField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     liked_by = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
-        return f'{self.author.username}: {self.content[:10]}'
-    
+        return f'{self.author.username}: {self.content[:20]}'
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
-    bio = models.TextField(max_length=160, blank=True, verbose_name="自己紹介")
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics', blank=True, null=True)
+    bio = models.TextField(max_length=160, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-# ユーザーが作られた時に、自動でプロフィールも作成されるようにする設定
+# ユーザー作成時にプロフィールを自動作成する信号
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
