61bd5d4ab0d1e54cf29283a23f1bafc22340bbb9
hal123w <haruto123c@gmail.com>
2026-05-30 16:04:08 +0900

    Add minimal tests for index login requirement

--- sns/tests.py
+++ sns/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    def test_index_requires_login(self):
+        response = self.client.get(reverse('sns:index'))
+        login_url = reverse('sns:login')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith(login_url))
+
+    def test_index_returns_200_when_logged_in(self):
+        User.objects.create_user(username='testuser', password='testpass123')
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(reverse('sns:index'))
+        self.assertEqual(response.status_code, 200)
